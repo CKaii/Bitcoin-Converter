8cb322ca70c44ca26b0177470bd16b13a063beca
unknown <kevin.cheung.kai@gmail.com>
2019-01-28 11:38:47 -0500

    complete cryptocurrency converter

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.")
+})
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+app.post("/", function(req, res) {
+  const crypto = req.body.crypto
+  const fiat = req.body.fiat
+  const amount = req.body.amount
+  const options = {
+    url: "https://apiv2.bitcoinaverage.com/convert/global",
+    method: "GET",
+    qs: {
+      from: crypto,
+      to: fiat,
+      amount:amount
+    }
+  }
+
+    request(options, function(error, response, body){
+      const data = JSON.parse(body);
+      const price = data.price;
+      const currentDate = data.time;
+
+      res.write("The current date is " + currentDate + "\n");
+      res.write(amount + crypto + " is currently worth " + price +" " + fiat);
+      res.send();
+    })
+})
